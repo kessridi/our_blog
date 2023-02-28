a469db8d7dfb755a1930b3261795d4dda91b047b
zahra <hammamizahrahammami@gmail.com>
2023-02-28 13:49:25 +0100

    ajout image

--- accueil.php
+++ accueil.php
@@ -3,14 +3,15 @@ require_once __DIR__ .'/utilities/Page.php';
 
 $indexPage = new Page();
 $msg ="hello";
-$tab =[
-'nom'=>'zahra',
-'prénom'=> 'hammami',
-'age'=>'27',
-];
+$nom ="hammami";
+$prenom ="zahra";
+$age = 27;
 $indexPage->renderPage('accueil.html.twig',[
     'msgAccueil'=>$msg,
-    'tab'=>$tab
-
+    'nom'=>$nom,
+    'prenom'=>$prenom,
+    'age'=>$age,
 ]);
+
+
 
